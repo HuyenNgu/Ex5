4b9fc162284559a5607d375944fb7adeec4b0036
GauCa <phanhuy0108@gmail.com>
2019-10-01 15:25:28 +0700

    Ceil and Floor

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void IsContrain5()
+        {
+
+        }
+        public static void IsContrain5()
+        {
+
+        }
+        public static void IsContrain5()
+        {
+
+        }
+        public static void IsContrain5()
+        {
+
+        }
+        public static void IsContrain5()
+        {
+
+        }
+    }   
+}
